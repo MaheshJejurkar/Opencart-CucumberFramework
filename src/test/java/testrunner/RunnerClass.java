c911c4896184170d555e055db4b32d7e1f220989
MaheshJejurkar <jejurkar.mahesh@gmail.com>
2024-10-04 22:37:16 +0530

    New code change

--- src/test/java/testrunner/RunnerClass.java
+++ src/test/java/testrunner/RunnerClass.java
@@ -3,28 +3,30 @@ package testrunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(	//features = {"src/test/java/features/"},
+@CucumberOptions(
+					features = {"src/test/java/features/registration.feature"}, 
+					glue = {"stepsdefinitions","hooks"},
+					plugin = {
+								"pretty", 
+								"html:reports/myreports.html",
+								"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+								//"rerun:target/rerun.txt"
+							},
+					dryRun = false,
+					monochrome = true,
+					publish = true
+					//features = {"src/test/java/features/"},
 					//features = {"src/test/java/features/login.feature"},
 					//features = {"src/test/java/features/login.feature", "src/test/java/features/login2.feature"},
-					//features = {"@target/rerun.txt"},
 					//features = {"src/test/java/features/loginDDTExcel.feature"},
 					//features = {"src/test/java/features/login2.feature"},
-					features = {"src/test/java/features/registration.feature"}, 
-					glue={"stepsdefinitions","hooks"},
-					plugin= {
-							"pretty", "html:reports/myreports.html",
-							"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-							//"rerun:target/rerun.txt"
-					},
-					dryRun=false,
-					monochrome=true,
-					publish=true
-					//tags="@sanity"
-					//tags="@regression"
-					//tags="@sanity and @regression"
-					//tags="@sanity and not @regression"
-					//tags="@sanity or @regression"
-					//tags="@regression and not @sanity"
+					//features = {"@target/rerun.txt"},
+					//tags = "@sanity"
+					//tags = "@regression"
+					//tags = "@sanity and @regression"
+					//tags = "@sanity or @regression"
+					//tags = "@sanity and not @regression"
+					//tags = "@regression and not @sanity"
 		)
 public class RunnerClass extends AbstractTestNGCucumberTests{
 
